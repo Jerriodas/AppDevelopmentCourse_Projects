a0348fab9c62897ca275535012be7bfb004dda9a
Gerardo Gordillo <33912419+Jerriodas@users.noreply.github.com>
2019-05-25 00:31:31 -0500

    Add files via upload
    An exercise to learn Tab Bar

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Jerriodas on 10/29/18.
+//  Copyright © 2018 Jerriodas. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBarItem.badgeValue = nil
+        tabBarItem.badgeColor = .orange
+        
+    }
+
+
+}
+
